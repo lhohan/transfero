b83ba738ec61ab21a60546960b7df8e7e68d13af
lhohan <github@lhoest.eu>
2013-04-06 18:37:46 +0200

    Chapter 5: ex 2 & 3

--- main/scala/scalaImpatient/ch05/Chapter05Classes.scala
+++ main/scala/scalaImpatient/ch05/Chapter05Classes.scala
@@ -17,4 +17,29 @@ object Chapter05Classes {
 
   }
 
+  //exercise 2
+  class BankAccount {
+
+    private var privateBalance = 0
+
+    def deposit(amount: Int) {
+      privateBalance += amount
+    }
+
+    def withdraw(amount: Int) {
+      privateBalance -= amount
+    }
+
+    def balance = this.privateBalance
+  }
+
+  //exercise 3
+  class Time(val hrs: Int, val min: Int) {
+    def before(other: Time): Boolean = {
+      if (hrs < other.hrs) true
+      else if (hrs == other.hrs && min < other.min) true
+      else false
+    }
+  }
+
 }
--- test/scala/scalaImpatient/ch05/Ch05ExercisesTest.scala
+++ test/scala/scalaImpatient/ch05/Ch05ExercisesTest.scala
@@ -2,19 +2,41 @@ package scalaImpatient.ch05
 
 import org.scalatest.FunSuite
 
-class Ch05ExercisesTest extends FunSuite{
+class Ch05ExercisesTest extends FunSuite {
 
   import scalaImpatient.ch05.Chapter05Classes._
 
   // exercise 1
-  test("test value Counter does not get incremented to negative value"){
-     val c : Counter = new Counter
+  test("test value Counter does not get incremented to negative value") {
+    val c: Counter = new Counter
     // quick implementation to increment to Max value
-    while(c.current != Int.MaxValue) c.increment()
+    while (c.current != Int.MaxValue) c.increment()
     assert(Int.MaxValue === c.current)
     // increment when MaxValue
     c.increment()
     assert(Int.MaxValue === c.current)
+  }
+
+  //exercise 2
+  test("test BankAccount class") {
+    val ba: BankAccount = new BankAccount
+    assert(0 === ba.balance)
+    ba.deposit(100)
+    assert(100 === ba.balance)
+    ba.withdraw(20)
+    assert(80 === ba.balance)
+  }
+
+  // exercise 3
+  test("test Time class") {
+    val t1 = new Time(0, 30)
+    val t2 = new Time(0, 45)
+    val t3 = new Time(0, 15)
+    val t4 = new Time(23, 15)
+
+    assert(t1.before(t2), "time test 1")
+    assert(!t1.before(t3), "time test 2")
+    assert(t1.before(t4), "time test 3")
   }
 
 }
