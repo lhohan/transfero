566221a44501943b1bd5216bbe88e58cdb2377ff
lhohan <github@lhoest.eu>
2013-04-08 13:57:49 +0200

    Chapter 6: ex 4 to 8

--- main/scala/scalaImpatient/ch06/Chapter06Objects.scala
+++ main/scala/scalaImpatient/ch06/Chapter06Objects.scala
@@ -1,6 +1,7 @@
 package scalaImpatient.ch06
 
-import java.awt.Point
+import java.awt.{Color, Point}
+import java.awt
 
 object Chapter06Objects {
 
@@ -32,9 +33,32 @@ object Chapter06Objects {
   object MilesToKilometers extends UnitConversion(1.609344)
 
   // exercise 3
-  object Origin extends Point(0,0){
+  object Origin extends awt.Point(0, 0) {}
+
+  // exercise 4
+  class Point(val x: Double, val y: Double)
+
+  object Point {
+    def apply(x: Double, y: Double) = new Point(x, y)
+  }
+
+  // exercise 6
+  object PlayingCard extends Enumeration {
+    type PlayingCard = Value
+    val ♣ = Value("\u2663")
+    val ♦ = Value("\u2666")
+    val ♥ = Value("\u2665")
+    val ♠ = Value("\u2660")
+
+    // exercise 7
+    def isRed(p: PlayingCard): Boolean = ((p == ♦) || (p == ♥))
 
   }
 
+  // exercise 8
+  object RGBColorCuberCorner extends Enumeration {
+    val RED = Value(0xff0000, "RED")
+    // ...
+  }
 
 }
--- test/scala/scalaImpatient/ch06/Ch06ExercisesTest.scala
+++ test/scala/scalaImpatient/ch06/Ch06ExercisesTest.scala
@@ -27,4 +27,30 @@ class Ch06ExercisesTest extends FunSuite {
     assert(0 === Origin.getX)
     assert(0 === Origin.getY)
   }
+
+  test("exercise 4: point creating using apply method") {
+    val p = Point(3, 4)
+    assert(3 === p.x)
+    assert(4 === p.y)
+  }
+
+  test("exercise 6 - playing cards enum") {
+    //    PlayingCard.values.foreach(println(_))
+    assert("♠" === PlayingCard.♠.toString)
+    assert("♦" === PlayingCard.♦.toString)
+    assert("♥" === PlayingCard.♥.toString)
+    assert("♠" === PlayingCard.♠.toString)
+  }
+
+  test("exercise 7 - is red test") {
+    assert(!PlayingCard.isRed(PlayingCard.♠))
+    assert(PlayingCard.isRed(PlayingCard.♦))
+    assert(PlayingCard.isRed(PlayingCard.♥))
+    assert(!PlayingCard.isRed(PlayingCard.♠))
+  }
+
+  test("exercise 8") {
+    assert(0xff0000 === RGBColorCuberCorner.RED.id)
+  }
+
 }
