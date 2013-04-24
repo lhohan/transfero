bb29829a108e7b10d6ae47362a4bb4f7ab4e4375
lhohan <github@lhoest.eu>
2013-04-24 14:07:15 +0200

    Chapter 10: ex 2

--- main/scala/scalaImpatient/ch10/Chapter10Traits.scala
+++ main/scala/scalaImpatient/ch10/Chapter10Traits.scala
@@ -1,6 +1,7 @@
 package scalaImpatient.ch10
 
 import java.awt.geom.Ellipse2D
+import java.awt.Point
 
 object Chapter10Traits {
 
@@ -14,6 +15,19 @@ object Chapter10Traits {
     def grow(dwith: Double, dheight: Double) {
       this.width += dwith
       this.height += dheight
+    }
+
+  }
+
+  class OrderPoint(x: Int, y: Int) extends java.awt.Point(x, y) with Ordered[Point] {
+
+    def compare(that: Point) = {
+      if ((x < that.getX || x == that.getX) && y < that.getY)
+        -1
+      else if (x == that.getX && y == that.getY)
+        0
+      else
+        1
     }
 
   }
--- test/scala/scalaImpatient/ch10/Ch10ExercisesTest.scala
+++ test/scala/scalaImpatient/ch10/Ch10ExercisesTest.scala
@@ -6,14 +6,21 @@ import scalaImpatient.ch10.Chapter10Traits._
 
 class Ch10ExercisesTest extends FunSuite {
 
-  test("exercise 1"){
-    val egg = new Ellipse2D.Double(5,10,20,30) with RectangleLike
-    egg.translate(10,-10)
-    egg.grow(100,200)
+  test("exercise 1") {
+    val egg = new Ellipse2D.Double(5, 10, 20, 30) with RectangleLike
+    egg.translate(10, -10)
+    egg.grow(100, 200)
     assert(15 === egg.getX)
     assert(0 === egg.getY)
     assert(120 === egg.getWidth)
     assert(230 === egg.getHeight)
+  }
+
+  test("exercise 2: same order") {
+    assert(new OrderPoint(2, 0).compare(new OrderPoint(1, 2)) > 0, "check 1")
+    assert(new OrderPoint(1,2).compare(new OrderPoint(1, 2)) == 0, "check 3")
+    assert(new OrderPoint(1, 1).compare(new OrderPoint(1, 2)) < 0, "check 2")
+    assert(new OrderPoint(0, 0).compare(new OrderPoint(1, 2)) < 0, "check 4")
   }
 
 }
