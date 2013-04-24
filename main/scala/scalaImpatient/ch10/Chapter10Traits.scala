2b475522c3b9ee059f0302b9b58b83ef65929d9e
lhohan <github@lhoest.eu>
2013-04-24 17:46:40 +0200

    Chapter 10: ex 4, 5

--- main/scala/scalaImpatient/ch10/Chapter10Traits.scala
+++ main/scala/scalaImpatient/ch10/Chapter10Traits.scala
@@ -2,9 +2,12 @@ package scalaImpatient.ch10
 
 import java.awt.geom.Ellipse2D
 import java.awt.Point
+import java.beans.{PropertyChangeEvent, PropertyChangeListener, PropertyChangeSupport}
+import java.beans
 
 object Chapter10Traits {
 
+  // exercise 1
   trait RectangleLike extends Ellipse2D.Double {
 
     def translate(dx: Double, dy: Double) {
@@ -19,6 +22,7 @@ object Chapter10Traits {
 
   }
 
+  // exercise 2
   class OrderPoint(x: Int, y: Int) extends java.awt.Point(x, y) with Ordered[Point] {
 
     def compare(that: Point) = {
@@ -31,5 +35,82 @@ object Chapter10Traits {
     }
 
   }
+
+  // exercise 4
+  trait Logger {
+    def log(msg: String)
+  }
+
+  class CryptoLogger(var cypher: Int = 3) extends Logger {
+
+    def encrypt(s: String): String = {
+      s.toCharArray.map {
+        c =>
+          val cyphered: Int = c.toInt + cypher
+          cyphered.toChar
+      }.mkString
+    }
+
+    def log(msg: String) {
+      println(encrypt(msg))
+    }
+
+  }
+
+  //exercise 5
+  trait PropertyChangeSupportLike {
+    private val support = new PropertyChangeSupport(this)
+
+    def addPropertyChangeListener(listener: PropertyChangeListener) {
+      support.addPropertyChangeListener(listener)
+    }
+
+    def removePropertyChangeListener(listener: PropertyChangeListener) {
+      support.removePropertyChangeListener(listener)
+    }
+
+    def getPropertyChangeListeners = support.getPropertyChangeListeners
+
+    def addPropertyChangeListener(propertyName: String, listener: PropertyChangeListener) {
+      support.addPropertyChangeListener(propertyName, listener)
+    }
+
+    def removePropertyChangeListener(propertyName: String, listener: PropertyChangeListener) {
+      support.removePropertyChangeListener(propertyName, listener)
+    }
+
+    def getPropertyChangeListeners(propertyName: String) = support.getPropertyChangeListeners
+
+    def firePropertyChange(propertyName: String, oldValue: Any, newValue: Any) {
+      support.firePropertyChange(propertyName, oldValue, newValue)
+    }
+
+    def firePropertyChange(propertyName: String, oldValue: Int, newValue: Int) {
+      support.firePropertyChange(propertyName, oldValue, newValue)
+    }
+
+    def firePropertyChange(propertyName: String, oldValue: Boolean, newValue: Boolean) {
+      support.firePropertyChange(propertyName, oldValue, newValue)
+    }
+
+    def firePropertyChange(event: PropertyChangeEvent) {
+      support.firePropertyChange(event)
+    }
+
+    def fireIndexedPropertyChange(propertyName: String, index: Int, oldValue: Any, newValue: Any) {
+      support.fireIndexedPropertyChange(propertyName, index, oldValue, newValue)
+    }
+
+    def fireIndexedPropertyChange(propertyName: String, index: Int, oldValue: Int, newValue: Int) {
+      support.fireIndexedPropertyChange(propertyName, index, oldValue, newValue)
+    }
+
+    def fireIndexedPropertyChange(propertyName: String, index: Int, oldValue: Boolean, newValue: Boolean) {
+      support.fireIndexedPropertyChange(propertyName, index, oldValue, newValue)
+    }
+
+    def hasListeners(propertyName: String) = support.hasListeners(propertyName)
+  }
+
 
 }
--- test/scala/scalaImpatient/ch10/Ch10ExercisesTest.scala
+++ test/scala/scalaImpatient/ch10/Ch10ExercisesTest.scala
@@ -3,6 +3,9 @@ package scalaImpatient.ch10
 import org.scalatest.FunSuite
 import java.awt.geom.Ellipse2D
 import scalaImpatient.ch10.Chapter10Traits._
+import java.awt.Point
+import java.beans.{PropertyChangeSupport, PropertyChangeEvent, PropertyChangeListener}
+import sun.reflect.generics.reflectiveObjects.NotImplementedException
 
 class Ch10ExercisesTest extends FunSuite {
 
@@ -16,11 +19,48 @@ class Ch10ExercisesTest extends FunSuite {
     assert(230 === egg.getHeight)
   }
 
-  test("exercise 2: same order") {
+  test("exercise 2") {
     assert(new OrderPoint(2, 0).compare(new OrderPoint(1, 2)) > 0, "check 1")
-    assert(new OrderPoint(1,2).compare(new OrderPoint(1, 2)) == 0, "check 3")
+    assert(new OrderPoint(1, 2).compare(new OrderPoint(1, 2)) == 0, "check 3")
     assert(new OrderPoint(1, 1).compare(new OrderPoint(1, 2)) < 0, "check 2")
     assert(new OrderPoint(0, 0).compare(new OrderPoint(1, 2)) < 0, "check 4")
   }
 
+  test("exercise 4 - default") {
+    val logger = new CryptoLogger() // default 3
+    assert("d" === logger.encrypt("a"))
+    assert("}" === logger.encrypt("z"))
+    assert("Fdhvdu" === logger.encrypt("Caesar"))
+  }
+
+  test("exercise 4 - with cypher -3") {
+    val logger = new CryptoLogger(-3) // default 3
+    assert("a" === logger.encrypt("d"))
+    assert("z" === logger.encrypt("}"))
+    assert("Caesar" === logger.encrypt("Fdhvdu"))
+  }
+
+  test("exercise 5") {
+    val p = new Point with PropertyChangeSupportLike {
+      // not sure if this how propertychangesupport is supposed to be triggered
+      // (expected something more automatic)
+      override def setLocation(x1: Int, y1: Int) {
+        firePropertyChange("location", x + "-" + y, x1 + "-" + y1)
+        x = x1
+        y = y1
+      }
+    }
+    // add a listener for testing purposes to just set a flag when a property changed
+    var detected = false
+    p.addPropertyChangeListener(new PropertyChangeListener {
+      def propertyChange(evt: PropertyChangeEvent) {
+        detected = true
+        //        println("change field " + evt.getPropertyName + " to " + evt.getNewValue)
+      }
+    })
+    assert(!detected)
+
+    p.setLocation(5, 7)
+    assert(detected)
+  }
 }
