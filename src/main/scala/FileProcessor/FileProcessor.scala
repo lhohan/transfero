5bebb697b5518c9f471dc224857fb828aee85c56
lhohan <github@lhoest.eu>
2013-09-11 21:34:45 +0200

    PathFilter type added, extensio filter used with hardcoded mda extension

--- src/main/scala/FileProcessor/FileProcessor.scala
+++ src/main/scala/FileProcessor/FileProcessor.scala
@@ -1,9 +1,7 @@
 
+
 import akka.actor._
 
-import FileProcessor._
-import FileProcessor.Monitor
-import FileProcessor.StartMonitoring
 import java.nio.file._
 import java.util.concurrent.atomic.AtomicInteger
 import scala.collection.mutable.ArrayBuffer
@@ -21,6 +19,64 @@ import scala.collection.JavaConversions._
  * and when files are found they are copied/moved to different location.
  *
  */
+
+
+package object transfero {
+
+  type PathFilter = Path => Boolean
+
+  case class StartMonitoring()
+
+  sealed trait FileCommand
+
+  case class CopyFile(file: Path, target: Path) extends FileCommand
+
+  case class MoveFile(file: Path, target: Path) extends FileCommand
+
+  case class Monitor()
+
+  case class MonitorConfig(srcLocation: String, targetLocation: String, processingType: ProcessingType, filter: PathFilter = {
+    path: Path => true
+  })
+
+
+  // logging
+  val IsDebugEnabled = true
+  val IsInfoEnabled = true
+
+  // quick log debug
+  def ld(msg: String) = {
+    // akka logger?
+    if (IsDebugEnabled) println("DEBUG " + msg)
+  }
+
+  // quick log info
+  def li(msg: String) = {
+    // akka logger?
+    if (IsInfoEnabled) println("INFO " + msg)
+  }
+
+  sealed trait ProcessingType {
+    def command(fileToProcess: Path, target: Path): FileCommand
+  }
+
+  case object Move extends ProcessingType {
+    override def command(file: Path, target: Path): FileCommand = {
+      new MoveFile(file, target)
+    }
+  }
+
+  case object Copy extends ProcessingType {
+    override def command(file: Path, target: Path): FileCommand = {
+      new CopyFile(file, target)
+    }
+  }
+
+
+}
+
+
+package transfero {
 
 object LocationMonitorMain extends App {
 
@@ -68,12 +124,19 @@ object LocationMonitorMain extends App {
       !_.startsWith("#")
     }
 
+    val ext: String = "mda"
+    val filter: PathFilter = {
+      path: Path =>
+        path.toString.endsWith(ext)
+    }
+
+
     val configs = new ArrayBuffer[MonitorConfig]()
     for (configLine <- lines) {
       ld("reading config line: " + configLine)
       val config: Array[String] = configLine.split(",")
       try {
-        configs += new MonitorConfig(config(1), config(2), toProcessingType(config(0)))
+        configs += new MonitorConfig(config(1), config(2), toProcessingType(config(0)), filter)
       } catch {
         case e: Throwable =>
           li("Error reading config line: \n" + configLine + "\n => Error is: " + e.getMessage)
@@ -84,41 +147,12 @@ object LocationMonitorMain extends App {
 
 }
 
-case class MonitorConfig(srcLocation: String, targetLocation: String, processingType: ProcessingType, filter: String = null)
-
 
 object FileProcessor {
 
   val system = ActorSystem("FileCopySystem")
   val fileCopier = system.actorOf(Props(new FileProcessor()), name = "file-copier")
   val monitorCounter = new AtomicInteger()
-
-  case class StartMonitoring()
-
-  sealed trait FileCommand
-
-  case class CopyFile(file: Path, target: Path) extends FileCommand
-
-  case class MoveFile(file: Path, target: Path) extends FileCommand
-
-  case class Monitor()
-
-
-  sealed trait ProcessingType {
-    def command(fileToProcess: Path, target: Path): FileCommand
-  }
-
-  case object Move extends ProcessingType {
-    override def command(file: Path, target: Path): FileCommand = {
-      new MoveFile(file, target)
-    }
-  }
-
-  case object Copy extends ProcessingType {
-    override def command(file: Path, target: Path): FileCommand = {
-      new CopyFile(file, target)
-    }
-  }
 
   class FileProcessor() extends Actor {
     def receive = {
@@ -139,28 +173,13 @@ object FileProcessor {
   def moveFileToLocation(file: Path, targetLocation: Path) = {
     Files.move(file, targetLocation.resolve(file.getFileName), StandardCopyOption.REPLACE_EXISTING)
   }
-
-
-  // logging
-  val IsDebugEnabled = true
-  val IsInfoEnabled = true
-
-  // quick log debug
-  def ld(msg: String) = {
-    // akka logger?
-    if (IsDebugEnabled) println("DEBUG " + msg)
-  }
-
-  // quick log info
-  def li(msg: String) = {
-    // akka logger?
-    if (IsInfoEnabled) println("INFO " + msg)
-  }
-
-
 }
 
 class FileProcessor(config: MonitorConfig) {
+
+  // TODO : I do not understand why I need to import this class' companion object...
+
+  import FileProcessor._
 
   val monitorLocation = Paths.get(config.srcLocation)
   val targetLocation = Paths.get(config.targetLocation)
@@ -174,12 +193,12 @@ class FileProcessor(config: MonitorConfig) {
 
   def monitor() {
     val locationMonitor = system.actorOf(Props(
-      new LocationMonitor(monitorLocation, targetLocation, config.processingType, fileCopier)
+      new LocationMonitor(monitorLocation, targetLocation, config.processingType, fileCopier, config.filter)
     ), name = "locationMonitor-" + monitorCounter.incrementAndGet())
     locationMonitor ! StartMonitoring()
   }
 
-  class LocationMonitor(location: Path, targetLocation: Path, processingType: ProcessingType, fileProcessor: ActorRef) extends Actor {
+  class LocationMonitor(location: Path, targetLocation: Path, processingType: ProcessingType, fileProcessor: ActorRef, pathFilter: PathFilter) extends Actor {
     val watcher = FileSystems.getDefault().newWatchService()
     location.register(watcher, ENTRY_CREATE)
 
@@ -189,13 +208,7 @@ class FileProcessor(config: MonitorConfig) {
         for (event <- watchKey.pollEvents()) {
           val path = event.context().asInstanceOf[Path]
           ld("watcher found: " + path)
-          val ext: String = "mda"
-          type PathFilter = Path => Boolean
-          val filter : PathFilter  = {
-            path: Path =>
-              path.toString.endsWith(ext)
-          }
-          if (filter(path)) {
+          if (pathFilter(path)) {
             fileProcessor ! processingType.command(location.resolve(path), targetLocation)
           } else {
             ld("... skipped, does not match filter")
@@ -210,14 +223,7 @@ class FileProcessor(config: MonitorConfig) {
     }
   }
 
-  trait PathFilter {
-    def filter(path: Path): Boolean
-  }
-
-  class ExtensionFilter(extension: String) extends PathFilter {
-    override def filter(path: Path): Boolean = {
-      path.toString.endsWith(extension)
-    }
-  }
+}
 
 }
+
